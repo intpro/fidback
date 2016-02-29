263ffbdeca1cfee5d9b63374e8e870075f6b111d
Boris <boris.dorofeev@gmail.com>
2016-02-29 17:06:01 +0600

    без adm

--- src/Laravel/Http/routes.php
+++ src/Laravel/Http/routes.php
@@ -1,7 +1,5 @@
 <?php
 
-Route::group(['middleware' => 'auth', 'prefix' => 'adm'], function()
-{
-    Route::get('/fidback/getform/{type}', ['as' => 'fidback_get',  'uses' => 'Interpro\Fidback\Laravel\Http\FidbackController@getForm']);
-    Route::post('/fidback/sendform',      ['as' => 'fidback_post', 'uses' => 'Interpro\Fidback\Laravel\Http\FidbackController@sendMessage']);
-});
+Route::get('/fidback/getform/{type}', ['as' => 'fidback_get',  'uses' => 'Interpro\Fidback\Laravel\Http\FidbackController@getForm']);
+Route::post('/fidback/sendform',      ['as' => 'fidback_post', 'uses' => 'Interpro\Fidback\Laravel\Http\FidbackController@sendMessage']);
+
